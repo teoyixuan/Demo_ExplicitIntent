cf03af0c4946262fe5984c95c4aafcc98a8845d6
18041430 <18041430@myrp.edu.sg>
2020-05-08 10:53:22 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoexplicitintent/Hero.java
@@ -0,0 +1,29 @@
+package sg.edu.rp.c346.demoexplicitintent;
+
+import java.io.Serializable;
+
+public class Hero implements Serializable {
+    private String name;
+    private int strength;
+    private int technicalProwess;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getTechnicalProwess() {
+        return technicalProwess;
+    }
+
+    public Hero(String name, int strength,
+                int technicalProwess) {
+        this.name = name;
+        this.strength = strength;
+        this.technicalProwess = technicalProwess;
+    }
+
+}
